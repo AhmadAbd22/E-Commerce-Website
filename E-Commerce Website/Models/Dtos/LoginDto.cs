195e260f7035a84982e4d03ccaa8023a071e68c0
AhmadAbd22 <ahmadbu648@gmail.com>
2025-08-04 13:28:39 +0500

    Made some changes

--- E-Commerce Website/Models/Dtos/LoginDto.cs
+++ E-Commerce Website/Models/Dtos/LoginDto.cs
@@ -1,8 +1,17 @@
-﻿namespace ECommerceWebsite.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceWebsite.Models.Dtos
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username")]
         public string Username { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
