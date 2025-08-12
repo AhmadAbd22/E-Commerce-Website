813d959a7d42598836e24adcef1aea2c814b1ea2
AhmadAbd22 <ahmadbu648@gmail.com>
2025-08-12 15:50:50 +0500

    Added more fields for pagination

--- E-Commerce Website/Models/Helping Classes/PagedResult.cs
+++ E-Commerce Website/Models/Helping Classes/PagedResult.cs
@@ -7,5 +7,9 @@
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
+        public int StartItem => TotalCount > 0 ? ((CurrentPage - 1) * PageSize) + 1 : 0;
+        public int EndItem => TotalCount > 0 ? StartItem + Items.Count - 1 : 0;
     }
 }
