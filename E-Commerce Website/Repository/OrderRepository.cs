3f5a9c2df791d17a7f54f0d0d0d909c26abc3fc2
AhmadAbd22 <ahmadbu648@gmail.com>
2025-08-20 18:53:33 +0500

    Added a method to get all user's count.

--- E-Commerce Website/Repository/OrderRepository.cs
+++ E-Commerce Website/Repository/OrderRepository.cs
@@ -1,5 +1,7 @@
 ﻿using ECommerceWebsite.Models;
 using ECommerceWebsite.Models.Context;
+using ECommerceWebsite.Models.Dtos;
+using ECommerceWebsite.Models.Helping_Classes;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceWebsite.Repository
@@ -16,6 +18,22 @@ namespace ECommerceWebsite.Repository
         //Update Order
         Task UpdateOrderAsync(OrderDetails order);
 
+        Task<PagedResult<OrderDetails>> GetAllOrdersPagedAsync(int pageNumber, int pageSize);
+
+        Task<int> GetTotalSales();
+
+        //total orders
+        Task<int> GetTotalOrders();
+
+        //pending orders
+        Task<int> GetPendingOrders();
+
+        // monthly basis sales
+        Task<decimal> GetMonthlySales();
+
+        //weekly basis sales
+        Task<decimal> GetWeeklySales();
+        Task<List<DailySalesDto>> GetWeeklySalesPerDay();
     }
     public class OrderRepository : IOrderRepository
     {
@@ -33,10 +51,10 @@ namespace ECommerceWebsite.Repository
 
         public async Task<List<OrderDetails>> GetOrderByUserIdAsync(Guid userId)
         {
-           return await _context.OrderDetails
-                .Where(o => o.UserId == userId)
-                .Include(o => o.OrderItems)
-                .ToListAsync();
+            return await _context.OrderDetails
+                 .Where(o => o.UserId == userId)
+                 .Include(o => o.OrderItems)
+                 .ToListAsync();
         }
 
         public async Task PlaceOrder(OrderDetails order)
@@ -55,5 +73,95 @@ namespace ECommerceWebsite.Repository
                 await _context.SaveChangesAsync();
             }
         }
+
+        #region methods for charts
+        public async Task<decimal> GetMonthlySales()
+        {
+            return await _context.OrderDetails
+                                 .Where(o => o.OrderStatus == "Completed" && o.OrderDate >= DateTime.Now.AddMonths(-1))
+                                 .SumAsync(o => o.TotalAmount);
+        }
+        public async Task<decimal> GetWeeklySales()
+        {
+            return await _context.OrderDetails
+                                .Where(o => o.OrderStatus == "Completed" && o.OrderDate >= DateTime.Now.AddDays(-7))
+                                .Select(o => (decimal?)o.TotalAmount)
+                                .SumAsync() ?? 0m;
+        }
+
+        public async Task<List<DailySalesDto>> GetWeeklySalesPerDay()
+        {
+            var today = DateTime.Today;
+            var sevenDaysAgo = today.AddDays(-6);
+
+            return await _context.OrderDetails
+                .Where(o => o.OrderStatus == "Completed" &&
+                            o.OrderDate.Date >= sevenDaysAgo &&
+                            o.OrderDate.Date <= today)
+                .GroupBy(o => o.OrderDate.Date)
+                .Select(g => new DailySalesDto
+                {
+                    Date = g.Key,
+                    TotalSales = g.Sum(o => o.TotalAmount)
+                })
+                .OrderBy(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<int> GetPendingOrders()
+        {
+            return await _context.OrderDetails
+                                    .Where(o => o.OrderStatus == "Pending")
+                                    .CountAsync();
+        }
+
+        public async Task<int> GetTotalOrders()
+        {
+            return await _context.OrderDetails
+                                 .CountAsync();
+        }
+
+        public async Task<int> GetTotalSales()
+        {
+            return await _context.OrderDetails
+                .Where(o => o.OrderStatus == "Completed")
+                .SumAsync(o => (int)o.TotalAmount);
+        }
+
+        #endregion
+
+        #region admin view order
+
+        public async Task<PagedResult<OrderDetails>> GetAllOrdersPagedAsync(int pageNumber, int pageSize)
+        {
+            var query = _context.OrderDetails
+                .Include(o => o.User) // Include the user data
+                .OrderByDescending(o => o.OrderDate)
+                .AsQueryable();
+
+            var totalCount = await query.CountAsync();
+
+
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            foreach (var order in items)
+            {
+                order.OrderItems = await _context.OrderItems
+                    .Include(oi => oi.Book)
+                    .Where(oi => oi.OrderDetailsId == order.Id)
+                    .ToListAsync();
+            }
+
+            return new PagedResult<OrderDetails>
+            {
+                Items = items,
+                CurrentPage = pageNumber,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+        }
+        #endregion
     }
 }
