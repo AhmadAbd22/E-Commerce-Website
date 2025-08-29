5d26148c98b0e9e20750444f031b1bbec938c2db
AhmadAbd22 <ahmadbu648@gmail.com>
2025-08-30 00:26:13 +0500

    Added a helping class and partial view for Toaster Notifications

---
+++ E-Commerce Website/Models/Helping Classes/TempDataExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace ECommerceWebsite.Models.Helping_Classes
+{
+    public static class TempDataExtensions
+    {
+        private const string SuccessKey = "Success";
+        private const string ErrorKey = "Error";
+        private const string InfoKey = "Info";
+        private const string WarningKey = "Warning";
+
+        public static void SetSuccess(this ITempDataDictionary tempData, string message)
+        {
+            tempData[SuccessKey] = message;
+        }
+
+        public static void SetError(this ITempDataDictionary tempData, string message)
+        {
+            tempData[ErrorKey] = message;
+        }
+
+        public static void SetInfo(this ITempDataDictionary tempData, string message)
+        {
+            tempData[InfoKey] = message;
+        }
+
+        public static void SetWarning(this ITempDataDictionary tempData, string message)
+        {
+            tempData[WarningKey] = message;
+        }
+    }
+
+}
