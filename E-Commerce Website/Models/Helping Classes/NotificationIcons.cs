b7a1fd97caaf4a75d472af2641c3e0630f62d841
AhmadAbd22 <ahmadbu648@gmail.com>
2025-09-01 21:33:11 +0500

    Added script helper to centralize common script references

---
+++ E-Commerce Website/Models/Helping Classes/NotificationIcons.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceWebsite.Models.Helping_Classes
+{
+    public static class NotificationIcons
+    {
+        public const string Success = "icon-checkmark3";
+        public const string Error = "icon-cross2";
+        public const string Warning = "icon-warning22";
+        public const string Info = "icon-info22";
+    }
+}
