9e5258588c83816995ebad6b78c0997ef68ff24c
AhmadAbd22 <ahmadbu648@gmail.com>
2025-09-04 11:27:47 +0500

    Made some changes to the names of TempData.

--- E-Commerce Website/Models/Helping Classes/TempDataExtensions.cs
+++ E-Commerce Website/Models/Helping Classes/TempDataExtensions.cs
@@ -4,30 +4,24 @@ namespace ECommerceWebsite.Models.Helping_Classes
 {
     public static class TempDataExtensions
     {
-        private const string SuccessKey = "Success";
-        private const string ErrorKey = "Error";
-        private const string InfoKey = "Info";
-        private const string WarningKey = "Warning";
-
         public static void SetSuccess(this ITempDataDictionary tempData, string message)
         {
-            tempData[SuccessKey] = message;
+            tempData["Success"] = message;
         }
 
         public static void SetError(this ITempDataDictionary tempData, string message)
         {
-            tempData[ErrorKey] = message;
+            tempData["Error"] = message;
         }
 
         public static void SetInfo(this ITempDataDictionary tempData, string message)
         {
-            tempData[InfoKey] = message;
+            tempData["Info"] = message;
         }
 
         public static void SetWarning(this ITempDataDictionary tempData, string message)
         {
-            tempData[WarningKey] = message;
+            tempData["Warning"] = message;
         }
     }
-
 }
