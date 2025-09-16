71b121cf7e7a5e727fb6de4ea959ed5c229d4a8f
AhmadAbd22 <ahmadbu648@gmail.com>
2025-09-16 13:54:14 +0500

    Added a 'RequestPathHelper.cs' to skip static files in Middleware Logging.

---
+++ E-Commerce Website/Models/Helping Classes/RequestPathHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceWebsite.Models.Helping_Classes
+{
+    public static class RequestPathHelper
+    {
+        private static readonly string[] skipPaths = { "/css", "/js", "/images", "/favicon.ico", "/assets" };
+        public static bool ShouldSkipLogging(PathString path)
+        {
+            return skipPaths.Any(skipPath => path.StartsWithSegments(skipPath));
+        }
+    }
+}
