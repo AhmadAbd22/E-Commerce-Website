f1c7883fd097d8954b638c3484d084df5b9da231
AhmadAbd22 <ahmadbu648@gmail.com>
2025-09-16 18:45:56 +0500

    Added a class to verify email format and domain.

---
+++ E-Commerce Website/Models/Helping Classes/EmailValidationHelper.cs
@@ -0,0 +1,41 @@
+﻿using DnsClient;
+using System.Net.Mail;
+
+namespace ECommerceWebsite.Models.Helping_Classes
+{
+    public static class EmailValidationHelper
+    {
+        public static bool IsEmailFormatValid(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static async Task<bool> IsDomainValidAsync(string email)
+        {
+            try
+            {
+                var domain = email.Split('@')[1];
+                var lookup = new LookupClient();
+                var result = await lookup.QueryAsync(domain, QueryType.MX);
+                return result.Answers.MxRecords().Any();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static async Task<bool> IsEmailValidAsync(string email)
+        {
+            return IsEmailFormatValid(email) && await IsDomainValidAsync(email);
+        }
+    }
+}
